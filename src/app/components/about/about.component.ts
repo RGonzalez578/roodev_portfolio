8c7ab0fb3cd3c2e536a97a325a3d3e243ba1e910
RGonzalez578 <ronald.chavesgonzalez@ucr.ac.cr>
2022-06-18 20:48:09 -0600

    Toggle section on about component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,33 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  @ViewChild('toggleBtn') toggleBtn!: ElementRef
+  @ViewChild('toggleIcon') toggleIcon!: ElementRef
+
   clicked: boolean = true
   recentProjects: boolean = true
   moreInfo: boolean = false
+  //toggler: string = 'transparent'
+  toggleTitle: string = 'Recent projects'
 
-  toggleSection(){
+  constructor(private renderer: Renderer2) {  }
+
+  ngOnInit(): void {  }
+
+  toggleSection(): void{
+
+    const toggler = this.toggleBtn.nativeElement
+    const icon = this.toggleIcon.nativeElement
+
     if(this.clicked){
       this.showInfo()
       this.clicked = false
+      this.toggleTitle = 'More about me'
+      this.renderer.setStyle(toggler, 'background-color', 'var(--orange)')
+      this.renderer.setStyle(icon, 'color', 'var(--white)')
+      //this.toggler = 'var(--orange)'
     }else{
       this.showRecentProjects()
       this.clicked = true
+      this.toggleTitle = 'Recent projects'
+      this.renderer.setStyle(toggler, 'background-color', 'transparent')
+      this.renderer.setStyle(icon, 'color', 'var(--orange)')
+      //this.toggler = 'transparent'
     }
   }
 
-  showRecentProjects(){
+  showRecentProjects(): void{
     this.recentProjects = true
     this.moreInfo = false
   }
 
-  showInfo(){
+  showInfo(): void{
     this.recentProjects = false
     this.moreInfo = true
-  }
-
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
 }
