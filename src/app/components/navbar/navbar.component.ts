0691f8aa69c35c1aab8b0f9aa6b0a215125c93f2
RGonzalez578 <ronald.chavesgonzalez@ucr.ac.cr>
2022-06-29 22:42:41 -0600

    Background navbar on mobile

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +9,7 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
 export class NavbarComponent implements OnInit {
 
   @ViewChild('mobileMenu', {static: false}) mobileMenu!: ElementRef
+  @ViewChild('backgroundNav', {static:false}) backgroundNav!: ElementRef
 
   mobileMenuShown: boolean = false
 
@@ -18,13 +20,23 @@ export class NavbarComponent implements OnInit {
 
   toggleMenu():void{
     const menu = this.mobileMenu.nativeElement
+    const background = this.backgroundNav.nativeElement
 
     if(this.mobileMenuShown){
-      this.renderer.setStyle(menu, 'top', '-23em')
+      
       this.mobileMenuShown = false
+      this.renderer.setStyle(background, 'opacity', '0')
+      this.renderer.setStyle(background, 'display', 'none')
+
+      this.renderer.setStyle(menu, 'top', '-23em')
     }else{
-      this.renderer.setStyle(menu, 'top', '0')
+      
       this.mobileMenuShown = true
+      this.renderer.setStyle(background, 'display', 'block')
+      this.renderer.setStyle(background, 'opacity', '0')
+      this.renderer.setStyle(background, 'opacity', '.5')
+
+      this.renderer.setStyle(menu, 'top', '0')
     }
     
   }
